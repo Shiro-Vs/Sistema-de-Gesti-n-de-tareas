c7f33515fdbb40b73fbb4b9e0873dd9bb1735e6c
ShiroVs <robert.vs@outlook.com>
2025-04-17 20:41:17 -0500

    "Actualizacoin de servidor (server.js)"

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require("path");
 
-// Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 8080;
 
-// Página principal (redireccionar a HTML/index.html)
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'HTML', 'index.html'));
+app.use(express.static(path.join(__dirname)));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "HTML/index.html"));
 });
 
-// Cualquier otra ruta no encontrada
-app.get('*', (req, res) => {
-  res.status(404).send('Página no encontrada');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
