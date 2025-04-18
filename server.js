4375a7d4eb9d812e4fbaeb66b234cd58b1f23ecb
ShiroVs <robert.vs@outlook.com>
2025-04-17 21:36:15 -0500

    "Servidor funcional terminado con paginas index e inicio(ruta de botones modificado)"

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "HTML/inicio.html"));
 });
 
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "HTML/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
